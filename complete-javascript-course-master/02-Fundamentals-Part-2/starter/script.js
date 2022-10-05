2b3911d085dfdd4396337f4034eb1e49c9546550
User <harshit.satya@perpetualblock.io>
2022-10-05 08:18:22 +0530

    new files

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,38 @@
+"use strict";
+/*
+let hasDriversLicence = false;
+const passTest = true;
+
+// if (passTest) hasDriverLicence = true; // script.js:6 Uncaught ReferenceError: hasDriverLicence is not defined
+// at script.js:6:32
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log("I can drive :D");
+
+// const interface = "audio"; //Uncaught SyntaxError: Unexpected strict mode reserved word (at script.js:9:7)
+*/
+
+////////////// Functions //////////////
+/*
+function logger() {
+  // defining the function
+  console.log("My name is Harshit.");
+}
+
+logger(); // calling / running / invoking the function.
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  //   console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+////////////// Functions //////////////
